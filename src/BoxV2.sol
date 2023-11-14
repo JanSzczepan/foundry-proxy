9e422452c59faee98fb5fe86b99dd802ce1de46a
Jan Szczepan <janszczepan62@gmail.com>
2023-11-14 20:36:14 +0100

    Make BoxV2 upgradable and ownable

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -2,8 +2,20 @@
 
 pragma solidity ^0.8.20;
 
-contract BoxV2 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+    }
 
     function setNumber(uint256 _number) external {
         number = _number;
@@ -16,4 +28,6 @@ contract BoxV2 {
     function getVersion() public pure returns (uint256) {
         return 2;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
