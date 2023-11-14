9226fa301aa94f5ee5f1d7f00af9cfdbca3bd015
Jan Szczepan <janszczepan62@gmail.com>
2023-11-14 20:34:11 +0100

    Make BoxV1 ownable

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -3,12 +3,18 @@
 pragma solidity ^0.8.20;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is UUPSUpgradeable {
+contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
 
     constructor() {
         _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
     }
 
     function getNumber() external view returns (uint256) {
